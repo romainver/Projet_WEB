b8d6603f956b99cafdcbe2e810b65a32fcce385b
romainver <romain.verhaeghe@viacesi.fr>
2017-04-20 09:47:26 +0200

    OCUserBundle

---
+++ projetWEB/src/OCUserBundle/Entity/User.php
@@ -0,0 +1,199 @@
+<?php
+namespace OCUserBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity()
+* @ORM\Table(name="utilisateur")
+*/
+class User implements UserInterface
+{
+  /**
+     * @ORM\Id @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $Idutilisateur;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $Nom;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $Prenom;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $Age;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $Promotion;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $Phrasechoc;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $Avatar;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    protected $Email;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $Motdepasse;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
+
+    protected $Categorie;
+    protected $roles;
+    protected $salt;
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+    public function setRoles($Roles)
+    {
+        $this->Roles=$Roles;
+    }
+
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+    public function setSalt($salt)
+    {
+        $this->salt=$salt;
+    }
+
+    public function getIdUtilisateur()
+    {
+        return $this->Idutilisateur;
+    }
+
+    public function setIdUtilisateur($Idutilisateur)
+    {
+        $this->IdUtilisateur = $Idutilisateur;
+    }
+
+    public function getNom()
+    {
+        return $this->Nom;
+    }
+
+    public function setNom($Nom)
+    {
+        $this->Nom = $Nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom($Prenom)
+    {
+        $this->Prenom = $Prenom;
+    }
+
+    public function getAge()
+    {
+        return $this->Age;
+    }
+
+    public function setAge($Age)
+    {
+        $this->Age = $Age;
+    }
+
+    public function getPromotion()
+    {
+        return $this->Promotion;
+    }
+
+    public function setPromotion($Promotion)
+    {
+        $this->Promotion = $Promotion;
+    }
+
+    public function getPhraseChoc()
+    {
+        return $this->Phrasechoc;
+    }
+
+    public function setPhraseChoc($Phrasechoc)
+    {
+        $this->Phrasechoc = $Phrasechoc;
+    }
+
+    public function getAvatar()
+    {
+        return $this->Avatar;
+    }
+
+    public function setAvatar($Avatar)
+    {
+        $this->Avatar = $Avatar;
+    }
+
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    public function setEmail($Email)
+    {
+        $this->Email = $Email;
+    }
+
+    public function getMotdepasse()
+    {
+        return $this->Motdepasse;
+    }
+
+    public function setMotdepasse($Motdepasse)
+    {
+        $this->Motdepasse = $Motdepasse;
+    }
+
+    public function getCategorie()
+    {
+        return $this->Categorie;
+    }
+
+    public function setCategorie($Categorie)
+    {
+        $this->Categorie = $Categorie;
+    }
+
+    public function eraseCredentials()
+
+  {
+
+  }
+
+  public function getPassword()
+  {
+    return $this->Motdepasse;
+  }
+  public function getUsername()
+  {
+    return $this->Prenom;
+  }
+}
