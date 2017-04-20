4b011fb0fb5f63bf333d0277ce67b81ea870128c
D-Peltre <dylan.peltre@gmail.com>
2017-04-20 13:16:23 +0200

    Essaie page profil

--- projetWEB/src/AppBundle/Controller/profile.php
+++ projetWEB/src/AppBundle/Controller/profile.php
@@ -4,16 +4,30 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 
 class profile extends Controller
 {
     /**
      * @Route("/profile", name="profile")
      */
-    public function profileAction()
+    public function profileAction(Request $request)
     {
-        return $this->render('profile.html.twig');
+    	$session = $request-> getSession();
+    	
+
+    	$user = $this->get('doctrine.orm.entity_manager')
+      ->getRepository('AppBundle:User')
+      ->findOneBy(array('Idutilisateur' => $session->get('iduser')));
+
+        return $this->render('profile.html.twig', array('user'=> $user));
     }
 }
